e4c2dbebeb5fbadb11fe0902d5ef564528c295c9
Paulo Siroy <pausiroy@gmail.com>
2022-08-07 04:17:13 -0700

    Add function to iterate through the buttons and add function to randomize computer choice.

--- app.js
+++ app.js
@@ -2,3 +2,16 @@ const player = document.querySelector("#playerText");
 const computer = document.querySelector("#computerText");
 const result = document.querySelector("#resultText");
 const choiceBtns = document.querySelectorAll(".choiceBtns");
+
+let player;
+let computer;
+let result;
+
+choiceBtns.forEach(button => button.addEventListener("click", () => {
+    player = button.textContent;
+    computerTurn();
+}))
+
+function computerTurn(){
+    const randNum = Math.floor(Math.random()*3) + 1
+}
