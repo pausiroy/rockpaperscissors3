b3b6780ce7a29c104d7dd0ee49f42b5f9c947975
Paulo Siroy <pausiroy@gmail.com>
2022-08-07 03:52:32 -0700

    Initialize HTML and grab using dom manipulation

---
+++ app.js
@@ -0,0 +1,4 @@
+const player = document.querySelector("#playerText");
+const computer = document.querySelector("#computerText");
+const result = document.querySelector("#resultText");
+const choiceBtns = document.querySelectorAll(".choiceBtns");
